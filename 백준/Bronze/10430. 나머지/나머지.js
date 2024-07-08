7d3f543ec8801de662c953364958722f65521356
GAMJAJeon <125572102+JeonGAMJA@users.noreply.github.com>
2024-07-08 21:42:46 +0900

    [Bronze V] Title: 나머지, Time: 128 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,6 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map((ele)=> Number(ele));
+
+console.log((input[0]+input[1])%input[2]);
+console.log(((input[0]%input[2])+(input[1]%input[2]))%input[2]);
+console.log((input[0]*input[1])%input[2]);
+console.log(((input[0]%input[2])*(input[1]%input[2]))%input[2]);
