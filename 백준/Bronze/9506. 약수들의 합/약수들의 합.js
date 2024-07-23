f46f69348c36d5046669768910cd848adf872220
GAMJAJeon <125572102+JeonGAMJA@users.noreply.github.com>
2024-07-23 18:23:54 +0900

    [Bronze I] Title: 약수들의 합, Time: 204 ms, Memory: 12316 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.js
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.js
@@ -3,4 +3,4 @@ const numbers = require('fs').readFileSync('/dev/stdin').toString().trim().split
 const factor = numbers.map((ele)=> Array.from({length:ele},(_,index)=> index).filter((num)=> ele%num===0))
 const sum = factor.map((ele)=>ele.reduce((acc,cur)=> acc+cur,0))
 
-return numbers.forEach((ele,index)=> ele===sum[index] ? console.log(ele+" = "+factor[index].join(" + ")):sum[index]>0 && console.log(ele+" is NOT perfect."))
+numbers.forEach((ele,index)=> ele===sum[index] ? console.log(ele+" = "+factor[index].join(" + ")):sum[index]>0 && console.log(ele+" is NOT perfect."))
