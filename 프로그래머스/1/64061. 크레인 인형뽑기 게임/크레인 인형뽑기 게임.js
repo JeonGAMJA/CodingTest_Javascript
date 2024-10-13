c66aebcb46ef4cd6c337b68cb7e2800e0b728cc3
GAMJAJeon <125572102+JeonGAMJA@users.noreply.github.com>
2024-10-14 00:48:09 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.73 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,5 +1,26 @@
-function solution(board, moves) {
-    return Array.from({length:board.length},(_,i)=> {
-        board.map((ele,index)=> ele)
-    })
-}
+const transpose = matrix =>
+    matrix.reduce(
+        (result, row) => row.map((_, i) => [...(result[i] || []), row[i]]),
+        []
+    );
+
+const solution = (board, moves) => {
+    const stacks = transpose(board).map(row =>
+        row.reverse().filter(el => el !== 0)
+    );
+    const basket = [];
+    let result = 0;
+
+    for (const move of moves) {
+        const pop = stacks[move - 1].pop();
+        if (!pop) continue;
+        if (pop === basket[basket.length - 1]) {
+            basket.pop();
+            result += 2;
+            continue;
+        }
+        basket.push(pop);
+    }
+
+    return result;
+};
