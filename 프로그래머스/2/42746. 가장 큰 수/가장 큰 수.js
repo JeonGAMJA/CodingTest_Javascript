8157a16acbd5f83c01dc0233c6228b51d32cb5d5
gamjajeon <125572102+JeonGAMJA@users.noreply.github.com>
2025-03-11 20:43:48 +0900

    [level 2] Title: 가장 큰 수, Time: 294.70 ms, Memory: 47.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -0,0 +1,8 @@
+const solution = (numbers) => {
+    const result = numbers
+        .map(String)
+        .sort((a, b) => (b + a).localeCompare(a + b))
+        .join('');
+    
+    return result[0] === '0' ? '0' : result;
+};
