976faa2305e8e34d1a232e01a9e55d40cfc3bee7
gamjajeon <125572102+JeonGAMJA@users.noreply.github.com>
2026-02-22 18:41:46 +0900

    [level 0] Title: 배열 만들기 3, Time: 10.23 ms, Memory: 42.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -0,0 +1,8 @@
+function solution(arr, intervals) {
+    return intervals.reduce(
+        (acc,cur)=> {
+            acc.push(arr.slice(cur[0],cur[1]+1))
+            return acc
+        },[]
+    ).flat();
+}
